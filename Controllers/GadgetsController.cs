9900e3e35b7ad332be6767e2aa9ba115e9a21270
VinhTQ <vinhtqde170661@fpt.edu.vn>
2025-06-02 12:29:34 +0700

    Implement API controllers

---
+++ Controllers/GadgetsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using OData.Data;
+
+namespace OData.Controllers
+{
+    [Route("odata/[controller]")]
+    public class GadgetsController : ODataController
+    {
+        private readonly MyWorldDbContext _myWorldDbContext;
+
+        public GadgetsController(MyWorldDbContext myWorldDbContext)
+        {
+            _myWorldDbContext = myWorldDbContext;
+        }
+
+        [EnableQuery]
+        [HttpGet("Get")]
+        public IActionResult Get()
+        {
+            return Ok(_myWorldDbContext.Gadgets.AsQueryable());
+        }
+    }
+}
