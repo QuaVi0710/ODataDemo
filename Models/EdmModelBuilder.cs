e577707e03317d1d2c744abee61048028b7c3635
VinhTQ <vinhtqde170661@fpt.edu.vn>
2025-06-02 12:29:01 +0700

    Add domain models

---
+++ Models/EdmModelBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+using System.Diagnostics.Metrics;
+
+namespace OData.Models
+{
+    public static class EdmModelBuilder
+    {
+        public static IEdmModel GetEdmModel()
+        {
+            ODataConventionModelBuilder modelBuilder = new ODataConventionModelBuilder();
+
+            modelBuilder.EntitySet<Gadgets>("Gadgets");
+
+            return modelBuilder.GetEdmModel();
+        }
+    }
+}
---
+++ Models/Gadgets.cs
@@ -0,0 +1,11 @@
+﻿namespace OData.Models
+{
+    public class Gadgets
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Brand { get; set; }
+        public decimal Cost { get; set; }
+        public string Type { get; set; }
+    }
+}
